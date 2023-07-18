104037b95e95a1d6756253152e00ee55af0bf8f0
Yaroslav Serdyukov <129312021+Ekleir@users.noreply.github.com>
2023-07-18 20:44:00 +0300

    Create guess-the-number.py

---
+++ guess-the-number.py
@@ -0,0 +1,25 @@
+# coding: utf8
+import random
+
+
+def guess_random_number():
+    ai_number = random.randint(1, 10)
+    user_number = int(input('Потрясающий Искуственный Интеллект загадал число от 1 до 10, угадай мои мысли.\n'))
+    count = 1
+    while ai_number != user_number:
+        if ai_number == user_number:
+            break
+        if user_number < ai_number:
+            count += 1
+            user_number = int(input('Маловато будет, ещё раз.\n'))
+        else:
+            count += 1
+            user_number = int(input('Перебор, лол, не угадал.\n'))
+    print(f'Бинго, это число {ai_number}')
+    return print('Быстро ты') if count < 3\
+        else print('Ну такой из тебя отгадыватель, средненький') if 2 < count <= 5\
+        else print('Ппц ты черепаха конечно')
+
+
+if __name__ == '__main__':
+    guess_random_number()
